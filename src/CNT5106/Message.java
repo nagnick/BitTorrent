5e0594d1a509facfce482bb7be46692efc091e66
nicolas <nicolasgarcia@ufl.edu>
2022-10-08 14:48:39 -0400

    Initial message class

--- src/CNT5106/Message.java
+++ src/CNT5106/Message.java
@@ -1,3 +1,21 @@
 package CNT5106;
 public class Message {
+    public enum MessageTypes{
+        choke,//0
+        unchoke,//1
+        interested,//2
+        notInterested,//3
+        have,//4
+        bitfield,//5
+        request,//6
+        piece//7
+    }
+    int length; // 4 bytes does not include itself first part in message
+    byte type; // 1 byte second part in message
+    String payload; // variable size message payload last in message
+    public Message(int length,byte type,String payload){
+        this.length = length;
+        this.type = type;
+        this.payload = payload;
+    }
 }
